d7f410cfb86caae7cb0ae543da95fa1fad91a140
Haroon <haroonsr@gmail.com>
2026-09-22 20:03:32 +0000

    test commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello from GitHub Actions CI/CD!");
+    res.end("Version 2 deployed using GitHub Actions!");
 });
 
 server.listen(3000, () => {
